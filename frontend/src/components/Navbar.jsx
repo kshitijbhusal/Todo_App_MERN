0c84c56ce2d9559e4bffa426f77d8a2442ec3336
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-02-14 22:23:36 +0545

    nav ui update and to card ui fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   };
   return (
     <div className="bg-[#31572C]">
-      <nav className="container mx-auto  text-[#ECF39E] h-[4rem] px-4 py-3 flex justify-between items-center">
+      <nav className="container mx-auto  text-[#ECF39E] h-[4rem]   flex justify-between items-center">
         <div className="text-xl font-bold  ">
           <Link to="/" className="flex">
             <h1 className="text-2xl">Mindleaf</h1>
@@ -45,26 +45,31 @@ const Navbar = () => {
               Account
             </Link>
           </li>
-          <li>
-            <Link
-              to="/signup"
-              className="text-lg px-4 py-2 text-black font-semibold rounded-lg   border-white bg-yellow-200 hover:bg-yellow-500 "
-            >
-              Sign Up
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/login"
-              className="text-lg px-4 py-2 text-black font-semibold rounded-lg   border-white bg-white hover:bg-blue-100"
-            >
-              Login
-            </Link>
-          </li>
+          {!user && !isLoggedIn && (
+            <li>
+              <Link
+                to="/signup"
+                className="text-lg px-4 py-2 text-black font-semibold rounded-lg   border-white bg-yellow-200 hover:bg-yellow-500 "
+              >
+                Sign Up
+              </Link>
+            </li>
+          )}
+
+          {!user && !isLoggedIn && (
+            <li>
+              <Link
+                to="/login"
+                className="text-lg px-4 py-2 text-black font-semibold rounded-lg   border-white bg-white hover:bg-blue-100"
+              >
+                Login
+              </Link>
+            </li>
+          )}
 
           {user && isLoggedIn && (
             <button>
-              <div className="h-12 w-12 bg-slate-500  text-white rounded-full flex justify-center items-center text-2xl  border-white uppercase ">
+              <div className="h-12 w-12 bg-green-600  text-white rounded-full flex justify-center items-center text-2xl  border-slate-200 border-2 uppercase ">
                 {user.username[0]}{" "}
               </div>
             </button>
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todos = ({ todos }) => {
+  const [completed, setCompleted] = useState(true);
   return (
     <div className="">
       {/* //----------------------------------Render Todos---------------------------------------------- */}
 
-      <div className="min-h-[calc(100vh-4rem)] py-5 drop-shadow-lg border-[#90A955] grid grid-cols-2 ">
+      <div className="min-h-[calc(100vh-4rem)] bg-red-500 py-5 drop-shadow-lg grid grid-cols-2 ">
         {todos.map((todo, index) => (
           <div
             key={index}
-            className="bg-white  m-2 w-80 border  rounded-lg px-4 py-2 hover:bg-slate-50 flex justify-between "
+            className="bg-white h-32  m-2 w-80 border  rounded-lg px-4 py-2 hover:bg-slate-50 flex flex-col justify-between "
           >
             <div>
-              <h1 className="text-lg font-semibold  text-black">
-                {todo.title}
-              </h1>
-              <p className=" font-semibold text-black ">
-                {" "}
-                - {todo.discription}
-              </p>
+              <div>
+                <h1 className="text-lg font-semibold  text-black">
+                  {todo.title}
+                </h1>
+                <hr />
+                <p className=" font-semibold text-black ">
+                  {" "}
+                  {todo.discription}
+                </p>
+              </div>
+              <button className="bg-slate-600 text-white px-2 py-2 text-[12px] rounded-md hover:bg-slate-800">
+                {todo.completed ? "Completed" : "Mark as completed"}
+              </button>
             </div>
-            <button className="bg-slate-600 text-white px-2 py-2 text-[12px] rounded-md hover:bg-slate-800">
-              {todo.completed ? "Completed" : "Mark as completed"}
-            </button>
+
+            <div className="bg-black  h-2 w-full rounded "></div>
           </div>
         ))}
       </div>
